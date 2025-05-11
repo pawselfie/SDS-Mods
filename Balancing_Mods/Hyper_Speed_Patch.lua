4dcde821dea1781541b9d2794689b42157e49935
pawselfie <hails12@pm.me>
2025-05-11 21:03:37 +0200

    upd

--- Balancing_Mods/Hyper_Speed_Patch.lua
+++ Balancing_Mods/Hyper_Speed_Patch.lua
@@ -42,21 +42,14 @@ local favgreatsword = "Great Windforce"
 --MakeWeapon("Arm Cannon", {ragdoll = 0, projectile = "Rocket", bombJumpMultiplier = 0.65, minDamage = 70, damage = 260, speed = 90, radius = 4.9}, {projectile = "Rocket", projectileSpeed = 90, chargeTime = 999})
 --MakeWeapon("Arm Cannon", {ragdoll = 0, projectile = "Rocket", bombJumpMultiplier = 0.65, minDamage = 70, damage = 200, speed = 25, radius = 7}, {projectile = "Rocket", projectileSpeed = 25, chargeTime = 999})
 --MakeWeapon("Arm Cannon", {ragdoll = 0, projectile = "Rocket", bombJumpMultiplier = 0.8, minDamage = 50, damage = 110, speed = 62, radius = 6}, {projectile = "Rocket", projectileSpeed = 50, chargeTime = 999})
---MakeWeapon("Arm Cannon", {ragdoll = 0, projectile = "CrystalRocket", bombJumpMultiplier = 0.65, minDamage = 140, damage = 260, speed = 75, radius = 6.5, detTime = 0.8}, {projectile = "CrystalRocket", projectileSpeed = 75, chargeTime = 999})
+MakeWeapon("Arm Cannon", {ragdoll = 0, projectile = "CrystalRocket", bombJumpMultiplier = 0.65, minDamage = 140, damage = 260, speed = 75, radius = 6.5, detTime = 0.51}, {projectile = "CrystalRocket", projectileSpeed = 75, chargeTime = 999})
 --MakeWeapon("Arm Cannon", {minDamage = 40, damage = 200, baseProjectile = "ShadowRocket", ragdoll = 0}, {projectile = "ShadowRocket"})
 
 
 MakeWeapon("Remote Detonator", nil, {reloadModifier = 0.1})
 MakeWeapon("Sticky Bomb", nil, {reloadModifier = 0.1})
 MakeWeapon("Sticky Potion", nil, {reloadModifier = 0.1})
-MakeWeapon("Shadow Bomb", nil, {reloadModifier = 0.5, noPlant = false})
-MakeWeapon("Bomb", nil, {reloadModifier = 0.5})
-MakeWeapon("Super Bomb", nil, {reloadModifier = 0.5}) --bomb skin
-MakeWeapon("Classic Bomb", nil, {reloadModifier = 0.5}) --bomb skin
-MakeWeapon("Bloom Bomb", nil, {reloadModifier = 0.5}) --bomb skin
-MakeWeapon("Square Bomb", nil, {reloadModifier = 0.5})
-MakeWeapon("Gift Bomb", nil, {reloadModifier = 0.5}) --square bomb skin
-MakeWeapon("Gem Bomb", nil, {reloadModifier = 0.5}) --square bomb skin
+MakeWeapon("Shadow Bomb", nil, {noPlant = false})
 
 MakeWeapon("Boomerang", nil, {reload = 3, chargeTime = 0})
 MakeWeapon("Bananarang", nil, {reload = 3, chargeTime = 0}) --boomerang skin
@@ -73,9 +66,6 @@ MakeWeapon("Lava Ball", nil, {reload = 1.8, chargeTime = 0})
 MakeWeapon("Jack-O-Lantern", nil, {reload = 1.8, chargeTime = 0}) --lavaball skin
 MakeWeapon("Cool Sun", nil, {reload = 1.8, chargeTime = 0}) --lavaball skin
 MakeWeapon("Snowball", nil, {reload = 0.8, chargeTime = 0})
-MakeWeapon("Paintball Gun", nil, nil)
-MakeWeapon("Ultra Soaker", nil, nil)
-MakeWeapon("Slingshot", nil, nil)
 MakeWeapon("Shadow Shuriken", nil, {reload = 1, reloadCharge = 1, chargeTime = 0})
 MakeWeapon("Wise Eye", nil, {reload = 999, reloadCharge = 9, chargeTime = 2, brickCost = "use regular shuri instead"})
 MakeWeapon("Pokey Ball", nil, {chargeTime = 0})
@@ -85,15 +75,15 @@ MakeWeapon(favsword, {ragdollResist = true, toolName = "Bridge Trowel"}, {reload
 MakeWeapon("Glowblade", {toolName = "Bridge Trowel"}, {reloadModifier = 999, swingSpeed = 999, lungeTime = 999, specialAttack = "use another sword insead", brickCost = "x"}) --disabled to disencourage unnecessary lag
 MakeWeapon("Umbrellade", {ragdollResist = true, toolName = "Bridge Trowel"}, {ragdollResist = true, reloadModifier = 0.3, lungeTime = 0, swingSpeed = 0.3})
 MakeWeapon("Petal Parasol", {ragdollResist = true, toolName = "Bridge Trowel"}, {ragdollResist = true, reloadModifier = 0.3, lungeTime = 0, swingSpeed = 0.3}) --umbrellade skin
-MakeWeapon("Tennis Racket", {ragdollResist = true, toolName = "Bridge Trowel", speed = 100, damage = 35, ragdoll = 1.4, knockback = Vector3.new(-40, 0, -40), reflectAccuracy = 999}, {reloadModifier = 0.25, swingSpeed = 0.3, ragdollResist = true, projectileSpeed = 100})
-MakeWeapon("Golden Racket", {ragdollResist = true, toolName = "Bridge Trowel", speed = 100, damage = 35, ragdoll = 1.4, knockback = Vector3.new(-40, 0, -40), reflectAccuracy = 999}, {reloadModifier = 0.25, swingSpeed = 0.3, ragdollResist = true, projectileSpeed = 100}) --tenis racket skin
-MakeWeapon("Ice Sword", {damage = 30, chill = 2.5, speed = 88, ragdollResist = true, toolName = "Bridge Trowel",}, {ragdollResist = true, projectileSpeed = 88, reloadModifier = 0.6, swingSpeed = 0.3})
+MakeWeapon("Tennis Racket", {ragdollResist = true, toolName = "Bridge Trowel", speed = 100, damage = 25, ragdoll = 1.4, knockback = Vector3.new(-40, 0, -40)}, {reloadModifier = 0.1, swingSpeed = 0.3, ragdollResist = true, projectileSpeed = 100})
+MakeWeapon("Golden Racket", {ragdollResist = true, toolName = "Bridge Trowel", speed = 100, damage = 25, ragdoll = 1.4, knockback = Vector3.new(-40, 0, -40)}, {reloadModifier = 0.1, swingSpeed = 0.3, ragdollResist = true, projectileSpeed = 100}) --tenis racket skin
+MakeWeapon("Ice Sword", {damage = 30, chill = 2.5, speed = 88, ragdollResist = true, toolName = "Bridge Trowel",}, {ragdollResist = true, projectileSpeed = 88, reloadModifier = 0.3333, swingSpeed = 0.3})
 MakeWeapon("Bamboo Buster", {ragdollResist = true, toolName = "Bridge Trowel", speed = 75, minDamage = 25, damage = 35, detTime = 0.8, bombJumpMultiplier = 0.75, noDestroy = false}, {ragdollResist = true, projectileSpeed = 75, reloadModifier = 0.6666, swingSpeed = 1.5, StrongSwing = false, noStrike = true})
 MakeWeapon("Brickbreaker", {ragdollResist = true, toolName = "Bridge Trowel"}, {ragdollResist = true, reloadModifier = 0.5, swingSpeed = 0.15})
 MakeWeapon("Batty Basher", {ragdollResist = true, toolName = "Bridge Trowel"}, {ragdollResist = true, reloadModifier = 0.5, swingSpeed = 0.15}) --brickbreaker skin
 MakeWeapon("Darkheart", {ragdollResist = true, toolName = "Bridge Trowel"}, {ragdollResist = true, reloadModifier = 0.666, swingSpeed = 0.666})
-MakeWeapon("Fire Sword", {ragdollResist = true, toolName = "Bridge Trowel", speed = 88, fireChance = 100}, {ragdollResist = true, projectileSpeed = 88, reloadModifier = 0.6, swingSpeed = 0.5})
-MakeWeapon("Firebrand", {ragdollResist = true, toolName = "Bridge Trowel", speed = 88, fireChance = 100}, {ragdollResist = true, projectileSpeed = 88, reloadModifier = 0.6, swingSpeed = 0.5}) --fire sword skin
+MakeWeapon("Fire Sword", {ragdollResist = true, toolName = "Bridge Trowel", speed = 88, fireChance = 100}, {ragdollResist = true, projectileSpeed = 88, reloadModifier = 0.3333, swingSpeed = 0.5})
+MakeWeapon("Firebrand", {ragdollResist = true, toolName = "Bridge Trowel", speed = 88, fireChance = 100}, {ragdollResist = true, projectileSpeed = 88, reloadModifier = 0.3333, swingSpeed = 0.5}) --fire sword skin
 MakeWeapon("Frying Pan", {ragdollResist = true, toolName = "Bridge Trowel", ragdoll = 1.4}, {ragdollResist = true, reloadModifier = 0.3333, swingSpeed = 0.5})
 MakeWeapon("Stop Sign", {ragdollResist = true, toolName = "Bridge Trowel", ragdoll = 1.4}, {ragdollResist = true, reloadModifier = 0.3333, swingSpeed = 0.5}) --frying pan skin
 MakeWeapon(favgreatsword, {ragdollResist = true, toolName = "Bridge Trowel"}, {ragdollResist = true, reloadModifier = 0.6666, swingSpeed = 1})
